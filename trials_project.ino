92232e322c6b1faa0792e38fde3a691584637ff9
noel-tem <noel.temores@sjsu.edu>
2025-09-13 22:49:41 -0700

    -condensed reading from registers - defined LSB sensitivity -combined some calculations to reduce lines of codes -configured baud -added a delay

--- trials_project.ino
+++ trials_project.ino
@@ -3,10 +3,11 @@
 #include <Servo.h>
 
 #define mpu 0x68
+#define LSB_SENS 16384.0
 
 Servo myservo;
 
-float offset = 0;
+float offset = 0.0;
 
 void setup() {
   //setting up mpu
@@ -16,7 +17,7 @@ void setup() {
   Wire.write(0); //waking up the mpu
   Wire.endTransmission(true);
 
-  Serial.begin(250000);
+  Serial.begin(19200);
 
   myservo.attach(9);
 
@@ -27,34 +28,26 @@ void loop() {
   Wire.beginTransmission(mpu);
   Wire.write(0x3b); //accelerometer output registers are 3b through 3f
   Wire.endTransmission();
-  Wire.requestFrom(mpu, 2); //requesting 2 bytes; gets both high and low for full 16 bit value
+  Wire.requestFrom(mpu, 6); //requesting 6 bytes, reading accelerometer data of all axes
   uint8_t ACCEL_X_H = Wire.read();
   uint8_t ACCEL_X_L = Wire.read();
-  int16_t ACCEL_X_OUT = (ACCEL_X_H << 8) | ACCEL_X_L; //combining high and low bits
-  float xAcc = (float)ACCEL_X_OUT / 16384.0; //converting raw data to g using LSB sens
-
-  //reading acceleration of y axis
-  Wire.beginTransmission(mpu);
-  Wire.write(0x3d);
-  Wire.endTransmission();
-  Wire.requestFrom(mpu, 2);
   uint8_t ACCEL_Y_H = Wire.read();
   uint8_t ACCEL_Y_L = Wire.read();
-  int16_t ACCEL_Y_OUT = (ACCEL_Y_H << 8) | ACCEL_Y_L; //combining high and low bits
-  float yAcc = (float)ACCEL_Y_OUT / 16384.0; //converting raw data to g using LSB sens
-
-  //reading acceleration of z axis (not needed for operation, but the trial project document asks for it)
-  Wire.beginTransmission(mpu);
-  Wire.write(0x3f);
-  Wire.endTransmission();
-  Wire.requestFrom(mpu, 2);
   uint8_t ACCEL_Z_H = Wire.read();
   uint8_t ACCEL_Z_L = Wire.read();
+
+  //combining high and low bits
+  int16_t ACCEL_X_OUT = (ACCEL_X_H << 8) | ACCEL_X_L;
+  int16_t ACCEL_Y_OUT = (ACCEL_Y_H << 8) | ACCEL_Y_L; 
   int16_t ACCEL_Z_OUT = (ACCEL_Z_H << 8) | ACCEL_Z_L;
-  float zAcc = (float)ACCEL_Z_OUT / 16384.0;
+
+  //converting raw data to g using LSB sens
+  float xAcc = (float)ACCEL_X_OUT / LSB_SENS; 
+  float yAcc = (float)ACCEL_Y_OUT / LSB_SENS;
+  float zAcc = (float)ACCEL_Z_OUT / LSB_SENS;
   
-  //computing acceleration due to gravity using angle between xAcc and yAcc
-  float gAcc = atan2(yAcc, xAcc);
+  //computing angle between y and x acceleration and cnoverting from radians to degrees
+  float gAngle = atan2(yAcc, xAcc) * (180.0 / PI);
 
   //getting offset from serial monitor
   if(Serial.available() > 0){ //if there are more than 0 bytes available to be read
@@ -62,12 +55,9 @@ void loop() {
     offset = input.toFloat();
   }
 
-  //converting from rads to degrees
-  float gDeg = gAcc * 180.0 / PI;
-
-  //calculating serco position
-  float servoDeg = 97.0 + gDeg + offset;
-  //               ^^^^ was 90 before, but the servo was a little off from pointing directly upwards;
+  //calculating servo position
+  float servoDeg = 97.0 + gAngle + offset;
+  //               ^^^^ was 90 before, but the servo was a little off from pointing directly upwards
   //                    this 7 degree addition came from testing with the serial monitor offset
 
   //printing to serial monitor
@@ -77,4 +67,5 @@ void loop() {
   Serial.println();
 
   myservo.write((int)servoDeg);
+  delay(10);
 }
